cefe8093e34f2295883ea2640810e684a21a97cd
licander <licand@mail.ru>
2017-08-25 02:17:45 +0300

    Add files via upload

--- homework3.4.py
+++ homework3.4.py
@@ -1,49 +1,27 @@
 # -*- coding: utf-8 -*-
+from pprint import pprint
+from urllib.parse import urlencode
 import requests
-import chardet
+token='c47ebd2e2dfab28ecd45605c463e728f3dfbea50783dda0548c5a1a4ef59a7ddbea2fe7c3915e160ed6e0'
+url = 'https://oauth.vk.com/authorize'
+APP_ID = '6160914'
+VERSION = '5.67'
+
+auth_data = {
+        'client_id': APP_ID,
+        'redirect_uri': 'https://oauth.vk.com/blank.html',
+        'display': 'mobile',
+        'scope': 'status',
+        'response_type': 'token',
+        'v': VERSION,
+        }
+print('?'.join((url, urlencode(auth_data))))
+
+params = {
+        'access_token': token,
+        'v': VERSION
+        }
 
 
-def translate_it(text, lang):
-    """
-    YANDEX translation plugin
-    docs: https://tech.yandex.ru/translate/doc/dg/reference/translate-docpage/
-    https://translate.yandex.net/api/v1.5/tr.json/translate ?
-    key=<API-ключ>
-     & text=<переводимый текст>
-     & lang=<направление перевода>
-     & [format=<формат текста>]
-     & [options=<опции перевода>]
-     & [callback=<имя callback-функции>]
-    :param text: <str> text for translation.
-    :return: <str> translated text.
-    """
-    url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-    key = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
-
-    params = {
-        'key': key,
-        'lang': lang,
-        'text': text,
-    }
-    response = requests.get(url, params=params).json()
-    return ' '.join(response.get('text', []))
-
-
-def read_from_file(file_name):
-    with open(file_name, 'rb') as file:
-        data = file.read()
-        coding = chardet.detect(data)
-        data = data.decode(coding['encoding'])
-    return data
-
-
-def main(input_file, output_file, current_lang, new_lang='ru'):
-    text = read_from_file(input_file)
-    lang = current_lang + '-' + new_lang
-    text = translate_it(text, lang)
-    with open(output_file, 'w') as file:
-        file.write(text)
-
-main('FR.txt', 'FR_new.txt', 'fr')
-main('DE.txt', 'DE_new.txt', 'de')
-main('ES.txt', 'ES_new.txt', 'es')
+response = requests.get('https://api.vk.com/method/status.get', params)
+pprint(response.json())
