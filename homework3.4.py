1988f3144d046b88e7a065af08c27b98a44478ae
licander <licand@mail.ru>
2017-08-26 03:45:25 +0300

    Add files via upload

--- homework3.4.py
+++ homework3.4.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from pprint import pprint
 from urllib.parse import urlencode
 import requests
-token='c47ebd2e2dfab28ecd45605c463e728f3dfbea50783dda0548c5a1a4ef59a7ddbea2fe7c3915e160ed6e0'
+from time import sleep
+
+token='32f45264a89d42fdbadc17d5a051dcab9beaf55473e03564f476426564308de4ac70abbc3c32998d45518'
 url = 'https://oauth.vk.com/authorize'
 APP_ID = '6160914'
 VERSION = '5.67'
@@ -11,17 +12,49 @@ auth_data = {
         'client_id': APP_ID,
         'redirect_uri': 'https://oauth.vk.com/blank.html',
         'display': 'mobile',
-        'scope': 'status',
+        'scope': 'friends',
         'response_type': 'token',
         'v': VERSION,
         }
-print('?'.join((url, urlencode(auth_data))))
+# print('?'.join((url, urlencode(auth_data))))
 
-params = {
+
+def get_friends_set(user_id=''):
+    params = {
         'access_token': token,
-        'v': VERSION
+        'v': VERSION,
+        'count': 1000,
+        'fields': 'nickname',
         }
+    if user_id:
+        params['user_id'] = user_id
+    response = requests.get('https://api.vk.com/method/friends.get', params)
+    res = response.json()
+    print(res)
+    if res.get('error'):
+        return 1
+    friend_set = set()
+    for friend in res['response']['items']:
+        friend_set.add(friend['id'])
+    return friend_set
 
 
-response = requests.get('https://api.vk.com/method/status.get', params)
-pprint(response.json())
+def get_general_friends():
+    my_friends = get_friends_set()
+    general_friends = set()
+    for num, friend_id in enumerate(my_friends):
+        print(friend_id)
+        res = get_friends_set(friend_id)
+        if res != 1:
+            if num == 0:
+                general_friends = get_friends_set(friend_id)
+                continue
+            else:
+                friends = get_friends_set(friend_id)
+                if friends != 1:
+                    general_friends = general_friends & friends
+        sleep(0.34)
+    return(general_friends)
+
+print(get_general_friends())
+# {167837}
