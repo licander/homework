41b0f5ac4ce5ee87b7f9339c911a22e69c43bbc9
licander <licand@mail.ru>
2017-09-19 19:09:46 +0300

    Add files via upload

--- stemmer.py
+++ stemmer.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from pymystem3 import Mystem
 from mean_text_from_url import get_mean_text_without_format
+from unique_text import get_serp
+import numpy as np
 text = get_mean_text_without_format('http://grand-lux.ru/shop/UID_18221.html')
 
 def text_to_unigram(text):
@@ -14,12 +16,14 @@ def text_to_unigram(text):
     return(lemmas)
 
 
-def find_unigram_in_url(query, url):
-    query_list = text_to_unigram(query)
+def find_unigram_in_url(query_list, url):
     text = get_mean_text_without_format(url)
     word_dict = dict()
     for word in query_list:
         word_dict[word] = 0
+        
+    if (type(text) == int):
+        return(word_dict)
     
     text_list = text_to_unigram(text)
     for word in text_list:
@@ -27,5 +31,18 @@ def find_unigram_in_url(query, url):
             word_dict[word] += 1
     return(word_dict)
 
+def get_top_mediana(query):
+    query_list = text_to_unigram(query)
+    serp = get_serp(query, '213', 10)
+    res = dict()
+    for word in query_list:
+        res[word] = list()
+    for url in serp:
+        unigrams = find_unigram_in_url(query_list, url)
+        for word in query_list:
+            res[word].append(unigrams[word])
+    for word, data in res.items():
+        res[word] = np.median(data)
+    print(res)
     
-print(find_unigram_in_url('автомобильный видеорегистратор', 'https://new-elektronika.ru/catalog/avtoelektronika-i-tehnika/videoregistratory/'))
+get_top_mediana('автомобильный видеорегистратор')
